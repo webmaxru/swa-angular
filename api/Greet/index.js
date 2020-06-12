33cae983c46ff071a805fa765ee583852975ee42
Maxim Salnikov <salnikov@gmail.com>
2020-06-12 12:24:26 +0200

    json

--- api/Greet/index.js
+++ api/Greet/index.js
@@ -1,13 +1,10 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? name
-        : "Static Web Apps";
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = name ? name : "Static Web Apps";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  context.res = {
+    body: { name: responseMessage },
+  };
+};

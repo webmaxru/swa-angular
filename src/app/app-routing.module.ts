8cd6608363a985e894ff08b0ccd34f0c461e70d5
Maxim Salnikov <salnikov@gmail.com>
2020-06-12 13:35:58 +0200

    links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { MeComponent } from './me/me.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'me', component: MeComponent },
 ];
 
 @NgModule({
